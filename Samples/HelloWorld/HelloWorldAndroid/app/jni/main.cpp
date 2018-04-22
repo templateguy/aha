6eea18c38a347e287289f60734db8003bc87f771
templateguy <31797443+templateguy@users.noreply.github.com>
2018-04-22 21:26:04 +0530

    added assimp/glm

---
+++ Samples/HelloWorld/HelloWorldAndroid/app/jni/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Priyanshi Thakur on 01/10/17.
+//
+
+#include <jni.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+JNIEXPORT void JNICALL Java_com_saurabh_helloworld_MainActivity_main()
+{
+
+}
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Samples/HelloWorld/HelloWorldAndroid/app/src/com/saurabh/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.saurabh.helloworld;
+
+
+import android.app.Activity;
+
+/**
+ * Created by priyanshi on 01/10/17.
+ */
+
+public class MainActivity extends Activity
+{
+
+}
